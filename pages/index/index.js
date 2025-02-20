e7944530366b1243ac50e13c2fba30645adf075a
duoduo <duoduo.lin@supwisdom.com>
2025-02-20 10:19:37 +0800

    initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,10 @@
+Page({
+  data: {
+    imgUrls: [
+      '/images/homeimage/sample1.jpeg',
+      '/images/homeimage/sample2.jpeg',
+      '/images/homeimage/sample3.jpeg',
+    ],
+    contentText: '在喧嚣都市中开辟一方静土，我们定期举办茶道研习、禅意手作、静心冥想等活动。通过质朴的空间设计与精心策划的内容，帮助现代人暂别纷扰，回归内心的平静与力量。'
+  }
+})
